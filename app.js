d2664f6298e8e1f36e97364ab644e2ee1c8cd02a
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-29 16:15:49 -0600

    Create the messages that will appear after each guess and win/lose message

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function game() {
 	availableLetters = "abcdefghijklmnopqrstuvwxyz";
 	lives = 6;
-	words = ["baseball", "ginandtonic", "ihopeyoulose", "beer", "generalassembly"];
+	wordBank = ["baseball", "ginandtonic", "ihopeyoulose", "beer", "generalassembly"];
+	messages = {
+		win: 'You win (probably cheated)!',
+		lose: 'You lose which seems about right',
+		guessed: 'already tried this letter loser, try again...',
+		validLetter: 'Please enter a letter from A-Z'
+	};
 }
 
